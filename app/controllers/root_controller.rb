6675b12919158ff2a1a6220681d3e2a667816119
shin-nagai <shintoiimasu@gmail.com>
2018-02-02 17:20:41 +0900

    インデントを綺麗にした

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,5 +1,5 @@
 class RootController < ApplicationController
 
-  def index
-  end
+   def index
+   end
 end
--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -6,94 +6,94 @@ class WarehousesController < ApplicationController
    def index
       @warehouses = Warehouse.all
       @hash = Gmaps4rails.build_markers(@warehouses) do |warehouse, marker|
-        marker.lat warehouse.latitude
-        marker.lng warehouse.longitude
-        # marker.infowindow warehouse.description
-        marker.infowindow render_to_string( partial: "warehouses/infowindow",
-                                    locals: { warehouse: warehouse} )
-        marker.json({name: warehouse.name})
+         marker.lat warehouse.latitude
+         marker.lng warehouse.longitude
+         # marker.infowindow warehouse.description
+         marker.infowindow render_to_string( partial: "warehouses/infowindow",
+            locals: { warehouse: warehouse} )
+            marker.json({name: warehouse.name})
+         end
       end
+      # GET /warehouses/1
+      # GET /warehouses/1.json
+      def show
+         @warehouses = Warehouse.find(params[:id])
+         @hash = Gmaps4rails.build_markers(@warehouses) do |warehouse, marker|
+            marker.lat warehouse.latitude
+            marker.lng warehouse.longitude
+            # marker.infowindow warehouse.description
+            marker.json({name: warehouse.name})
+            @reservation = Reservation.new
+         end
+      end
+
+      def done
+         @warehouse = Warehouse.limit(1).order("created_at DESC")
+         @hash = Gmaps4rails.build_markers(@warehouses) do |warehouse, marker|
+            marker.lat warehouse.latitude
+            marker.lng warehouse.longitude
+            # marker.infowindow warehouse.description
+            marker.json({name: warehouse.name})
+         end
+      end
+
+      # GET /warehouses/new
+      def new
+         @warehouse = Warehouse.new
+      end
+
+      # GET /warehouses/1/edit
+      def edit
+      end
+
+      # POST /warehouses
+      # POST /warehouses.json
+      def create
+         @warehouse = Warehouse.new(warehouse_params)
+         respond_to do |format|
+            if @warehouse.save
+               format.html { redirect_to :action=>'done', notice: 'Warehouse was successfully created.' }
+               format.json { render :show, status: :created, location: @warehouse }
+            else
+               format.html { render :new }
+               format.json { render json: @warehouse.errors, status: :unprocessable_entity }
+            end
+         end
+      end
+
+      # PATCH/PUT /warehouses/1
+      # PATCH/PUT /warehouses/1.json
+      def update
+         respond_to do |format|
+            if @warehouse.update(warehouse_params)
+               format.html { redirect_to @warehouse, notice: 'Warehouse was successfully updated.' }
+               format.json { render :show, status: :ok, location: @warehouse }
+            else
+               format.html { render :edit }
+               format.json { render json: @warehouse.errors, status: :unprocessable_entity }
+            end
+         end
+      end
+
+      # DELETE /warehouses/1
+      # DELETE /warehouses/1.json
+      def destroy
+         @warehouse.destroy
+         respond_to do |format|
+            format.html { redirect_to warehouses_url, notice: 'Warehouse was successfully destroyed.' }
+            format.json { head :no_content }
+         end
+      end
+
+      private
+      # Use callbacks to share common setup or constraints between actions.
+      def set_warehouse
+         @warehouse = Warehouse.find(params[:id])
+      end
+
+      # Never trust parameters from the scary internet, only allow the white list through.
+      def warehouse_params
+         params.require(:warehouse).permit(:name, :adress, :price)
+      end
+
    end
-  # GET /warehouses/1
-  # GET /warehouses/1.json
-  def show
-     @warehouses = Warehouse.find(params[:id])
-     @hash = Gmaps4rails.build_markers(@warehouses) do |warehouse, marker|
-      marker.lat warehouse.latitude
-      marker.lng warehouse.longitude
-      # marker.infowindow warehouse.description
-      marker.json({name: warehouse.name})
-      @reservation = Reservation.new
-     end
-  end
-
-  def done
-     @warehouse = Warehouse.limit(1).order("created_at DESC")
-     @hash = Gmaps4rails.build_markers(@warehouses) do |warehouse, marker|
-      marker.lat warehouse.latitude
-      marker.lng warehouse.longitude
-      # marker.infowindow warehouse.description
-      marker.json({name: warehouse.name})
-     end
-  end
-
-  # GET /warehouses/new
-  def new
-    @warehouse = Warehouse.new
-  end
-
-  # GET /warehouses/1/edit
-  def edit
-  end
-
-  # POST /warehouses
-  # POST /warehouses.json
-  def create
-    @warehouse = Warehouse.new(warehouse_params)
-    respond_to do |format|
-      if @warehouse.save
-        format.html { redirect_to :action=>'done', notice: 'Warehouse was successfully created.' }
-        format.json { render :show, status: :created, location: @warehouse }
-      else
-        format.html { render :new }
-        format.json { render json: @warehouse.errors, status: :unprocessable_entity }
-      end
-   end
-  end
-
-  # PATCH/PUT /warehouses/1
-  # PATCH/PUT /warehouses/1.json
-  def update
-    respond_to do |format|
-      if @warehouse.update(warehouse_params)
-        format.html { redirect_to @warehouse, notice: 'Warehouse was successfully updated.' }
-        format.json { render :show, status: :ok, location: @warehouse }
-      else
-        format.html { render :edit }
-        format.json { render json: @warehouse.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /warehouses/1
-  # DELETE /warehouses/1.json
-  def destroy
-    @warehouse.destroy
-    respond_to do |format|
-      format.html { redirect_to warehouses_url, notice: 'Warehouse was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_warehouse
-      @warehouse = Warehouse.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def warehouse_params
-      params.require(:warehouse).permit(:name, :adress, :price)
-    end
-
-end
